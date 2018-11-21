afb7672dd67c8e873eb9b2f76f870e506d256b19
Bagus Candradinatha <candradinatha>
2018-11-22 05:23:50 +0800

    test

--- big-data.py
+++ big-data.py
@@ -10,7 +10,7 @@ access_token_secret = "97GvmsPUzASgkLDEAhzhIf6nholUc3JpiYEUre8nXjj4t"
 
 name_var = ""
 jumlah_data = 100
-
+x = {}
 
 def connectData(ArgsConsKey,ArgsConsSecret,ArgsToken,ArgsTokenSecret):
     auth = tweepy.OAuthHandler(ArgsConsKey,ArgsConsSecret)
@@ -20,20 +20,22 @@ def connectData(ArgsConsKey,ArgsConsSecret,ArgsToken,ArgsTokenSecret):
     return api
 
 def getData(ArgsApi,hashtagArgs):
-    x = []
+    global x
     count = 0
 
-    for status in tweepy.Cursor(ArgsApi.search,q=hashtagArgs).items(20):
+    for status in tweepy.Cursor(ArgsApi.search,q=hashtagArgs).items(2):
 
         # x.update(status._json)
         print(status._json)
-        x.append(status._json)
+
+        x[hashtagArgs] = status._json
+
         count+=1
         print(count)
 
 
 
-
+def save_to_json():
     with open(name_var+'.json', 'a+') as f:
         print("create file")
         json.dump(x, f,indent=2)
@@ -41,12 +43,15 @@ def getData(ArgsApi,hashtagArgs):
 
 
 
+
 def main():
+    global x
     global  name_var
     myApi = connectData(consumer_key, consumer_secret, access_token, access_token_secret)
     f= open("hashtag.txt","r")
 
     for i in f:
+        x = {}
         tgl_skrng = datetime.datetime.now()
         hashtags = i.split(",")
         judul = hashtags[0]+'%s'%(tgl_skrng.microsecond)
@@ -54,6 +59,9 @@ def main():
         for ht in hashtags:
             print(ht)
             getData(myApi,ht)
+
+        save_to_json()
+
     pass
 
 
